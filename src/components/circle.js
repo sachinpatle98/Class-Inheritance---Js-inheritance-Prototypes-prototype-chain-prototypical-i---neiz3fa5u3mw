e10bdf4347e07ac24402c01f4adeeba61d35c241
Purushottam Patle <patlep08@gmail.com>
2022-10-28 10:22:08 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
-
+var Shape = require('.Shape.js)
+                  
 // declare class
+ Class Circle extends Shape {
+                    constructor(color) {
+  super();
+}
+calculateArea() {
+}
+}
 
 
 // export class using module.exports
+module.exports = Circle;
